f6771a2a5f0a4598566bafd3a8d78093ce020345
j-gabbard <74122274+j-gabbard@users.noreply.github.com>
2020-12-03 09:10:15 -0600

    First draft of day 3 problem 1

---
+++ day3_toboggan.py
@@ -0,0 +1,39 @@
+import re 
+
+
+def open_file():
+  #going to open file and clean up data
+  with open('./inputs/day3.txt', 'r') as f:
+    scrubbed = []
+    for line in f:
+      scrubbed.append(line.strip())
+    #print(scrubbed)
+    return scrubbed
+
+def move_toboggan(right, file):
+  #takes number of spaces to move right per line
+  #then checks for collision with tree (marked as '#')
+  index = -3
+  collisions = 0
+  skip_first = 0
+  
+  for line in file:
+    #increment index by number of spaces right per iteration
+    index += right
+    if skip_first == 0:
+      skip_first += 1
+    else:
+      skip_first += 1
+      #there are 31 characters per line, find modulo when it wraps. Need to
+      #subtract one to make sure 0th index = first character
+      position = (index % 31) 
+      print(f'position = {position}')
+      print(line)
+      if line[position] == '#':
+        collisions += 1
+        print(f'collision detected on line {skip_first}, position {position}')
+
+    
+  print(collisions) 
+    
+move_toboggan(3, open_file())
