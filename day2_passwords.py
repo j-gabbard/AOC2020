1ad36862401590f77a2bf3ab226a41d3ce18d4ed
j-gabbard <74122274+j-gabbard@users.noreply.github.com>
2020-12-02 18:27:49 -0600

    First challenge solved.

--- day2_passwords.py
+++ day2_passwords.py
@@ -1,5 +1,6 @@
 import re 
 
+
 def open_file():
   #going to open file and clean up data
   with open('./inputs/day2.txt', 'r') as f:
@@ -9,14 +10,55 @@ def open_file():
     #print(scrubbed)
     return scrubbed
 
-def password_check(passwords):
+'''def consecutive_password_check(passwords):
+  counter = 0
   for line in passwords:
-    quantity = line.split()[0]
+    quantity = line.split()[0].replace('-', ',')
     character = re.search('[a-z](?=[:])', line).group()
     password = line.split()[-1]
-    #print(quantity)
-    #print(characterraw)
-    #print(password)
+    regex = character + '{' + f'{quantity}' + '}' 
+
     
+    results = re.search(rf'{regex}', password)
+    if results:
+      #print(results.group())
+      counter += 1
+    #print(regex)
+    #print(quantity)
+    #print(character)
+    #print(password)
+  print(counter)
+  
+    
+password_check(open_file())'''
+
+
+def password_check(passwords):
+  line_counter = 0
+  for line in passwords:
+    character_counter = 0
+    #quantity is last part of line
+    quantity = line.split()[0]
+    
+    quantity_list = quantity.split('-')
+    quantity_low = int(quantity_list[0])
+    quantity_high = int(quantity_list[1]) + 1
+    
+    #print(quantity)
+    #print(quantity_low)
+    #print(quantity_high) 
+    
+    character = re.search('[a-z](?=[:])', line).group()
+    password = line.split()[-1]
+    regex = character + '{' + f'{quantity}' + '}' 
+    
+    for letter in password:
+      if letter == character:
+        character_counter += 1
+    
+    if character_counter in range(quantity_low, quantity_high):
+      line_counter += 1
+
+  print(line_counter)   
+  
 password_check(open_file())
-   
