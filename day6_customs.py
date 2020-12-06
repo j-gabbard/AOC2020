13001c71199575970262620f8eb31b8bb92acc39
j-gabbard <74122274+j-gabbard@users.noreply.github.com>
2020-12-06 11:01:58 -0600

    Part 2 complete

--- day6_customs.py
+++ day6_customs.py
@@ -1,6 +1,6 @@
 import re 
 
-def open_file():
+def open_file_scrubbed():
   #going to open file and clean up data
   with open('./inputs/day6.txt', 'r') as f:
     scrubbed = []
@@ -8,8 +8,20 @@ def open_file():
     scrubbed = [line.replace('\n','') for line in lines]
 
     return scrubbed
+    
+def open_file_raw():
+  #going to open file and only clean double newline, replace
+  #newline with &
+  with open('./inputs/day6.txt', 'r') as f:
+    scrubbed = []
+    lines = f.read().split("\n\n")
+    scrubbed = [line.replace('\n','&') for line in lines]
 
-def sum_answers(input_data):
+
+    return scrubbed
+
+#first answer:
+def sum_unique_answers(input_data):
   #print(input_data)
   total_sum = 0
   #for group in input_data:
@@ -27,4 +39,19 @@ def sum_answers(input_data):
   
   print(total_sum)
 
-sum_answers(open_file())
+#second answer
+def sum_agree_answers(input_data):
+  total_sum = 0
+  
+  for group in input_data:
+    yes_answers = []
+    print(group)
+    for answer in group:
+      if (group.count(answer) == group.count('&') + 1 and 
+        answer not in yes_answers and answer != '&'):
+        yes_answers.append(answer)
+        total_sum += 1
+    
+  print(total_sum)
+
+sum_agree_answers(open_file_raw())
