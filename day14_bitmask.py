00ecebb954865ad1adec273988ba2758f8ddbc60
j-gabbard <74122274+j-gabbard@users.noreply.github.com>
2020-12-15 10:24:44 -0600

    Cleaned up code, final day 14

--- day14_bitmask.py
+++ day14_bitmask.py
@@ -11,7 +11,6 @@ def open_file():
     #ordered = sorted(scrubbed)
     return scrubbed
 
-#print(open_file())
 
 def bitmask(mask, inp):
   bin_inp = format(inp, 'b').zfill(36)
@@ -24,22 +23,16 @@ def bitmask(mask, inp):
       temp += '1'
     elif mask[index] == '0':
       temp += '0'
-  #print(f'len(mask) = {len(mask)}, len(input) = {len(bin_inp)}, len(out) = {len(temp)}')
   return temp
   
-#print(bitmask('11X00100011111110000X1010X001X010000', 101))
 
 def mem_mask(mask, address):
-  off = []
-  on = []
   temp = []
   output = []
   counter = 0
   bin_address = format(int(address), 'b').zfill(36)
   mask = mask.zfill(36)
-  
-  print(bin_address, mask)
-  
+    
   for index, digit in enumerate(bin_address):    
     if mask[index] == '1':
       temp.append('1')
@@ -48,29 +41,21 @@ def mem_mask(mask, address):
       counter += 1
     elif mask[index] == '0':
       temp.append(digit)
-    print(mask[index], temp[-1])
-  print(temp)
   #need to convert back to a string
   temp = ''.join(temp)
-  print(len(temp), temp)
   if counter == 0:
     return ''.join(temp)
     
   combos = list(itertools.product([0, 1], repeat=counter))
   
-  #print(combos)
   for combo in combos:
     tempout = temp
     for x in combo:
       tempout = tempout.replace('X', str(x), 1)
-    #print(tempout)
     output.append(tempout)
   
-  #print(bin_address, temp)
-  print(output)
   return output
   
-mem_mask('000000000000000000000000000000X1001X', '42')
 
 
 
@@ -97,19 +82,16 @@ def part2(inp):
       mask = value
     if cmd.split('[')[0] == 'mem':
       masked = mem_mask(mask, cmd[4:].split(']')[0])
-      #print(masked)
       for index in masked:
         mem[int(index, 2)] = int(value)
       continue
   
   print(sum(mem.values()))
   
-  
-  
-  #print(masked)
-  
+
+
   
 t = time.time()
-#part1(open_file())  
+part1(open_file())  
 part2(open_file())   
-print(time.time()-t)
+print(f'took {time.time()-t} seconds')
