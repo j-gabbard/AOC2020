237cf08027a21054fb64e885d67be1ff5ea2ce6b
j-gabbard <74122274+j-gabbard@users.noreply.github.com>
2020-12-16 14:29:48 -0600

    cleaned up day 16
    `

--- day16_train_tickets.py
+++ day16_train_tickets.py
@@ -1,9 +1,7 @@
 import time
 import re
-import itertools
 import math
-
-mem = {}
+import timeit 
 
 def open_file():
   #going to open file and clean up data
@@ -43,8 +41,8 @@ def find_invalid(inp):
   invalids = []
   valids = []
   for line in inp:
-    if re.match(r'^[0-9]', line):
-      numbers = [int(x) for x in line.split(',')]
+    if re.match(r'^[0-9]', line): #find lines beginning with number
+      numbers = [int(x) for x in line.split(',')] 
       flag = 1
       for number in numbers:
         allowed = []
@@ -55,43 +53,31 @@ def find_invalid(inp):
           flag = 0
       if flag == 1:
         valids.append(line)
-  #print(sum(invalids))
   valids_out = []
   for x in valids:
-    #valids_out.append(int(y) for y in x.split(','))
     temp = x.split(',')
     valids_out.append([int(y) for y in temp])
-    #print(temp)
-    #hold_on = input()
-  #print(valids_out)
+  #used same function to find invalids for part1 but recycled
   return(valids_out)
+
 
 ticket_fields = dict()
 data = generate_data(open_file())
 
-
 def find_categories_part_2(valid_tickets):
   global ticket_fields
   global data
-  data1 = [x for x in data]
-
-  
-  #print(data)
+  data1 = [x for x in data] #this ended up not being necessary
   counter = 1
+  
   while valid_tickets[0]: #using [0] since will contain a list of empty lists
     valid_field = [x.pop(0) for x in valid_tickets]
-    #print(valid_tickets)
-    #print(valid_field)
-    #hold_on = input()
     uniques = []
     uniques.extend([x for x in valid_field if x not in uniques])
     flag = 0
     match = ''
     
     for k, v in data.items():
-      #print(v)
-      #print(set(uniques))
-      #print(set(v))
       if set(uniques).issubset(v):
         #print(f'hay match - {counter} and {k}')
         match = k
@@ -102,8 +88,6 @@ def find_categories_part_2(valid_tickets):
       del data[match]
     
     counter += 1
-    #print(counter)
-  print(ticket_fields)
     
   if data:
     if data1 == data:
@@ -111,22 +95,17 @@ def find_categories_part_2(valid_tickets):
       exit()
     else:
       find_categories_part_2(find_invalid(open_file()))
-    
-      
 
 
-  
-  #print(valid_field)
-
-
-def get_my_departure_product(my_ticket):
+def get_my_departure_product():
   global ticket_fields
+  my_ticket = [73,101,67,97,149,53,89,113,79,131,71,127,137,61,139,103,83,107,109,59]
   my_fields = []
   for k, v in ticket_fields.items():
     if re.match(r'^departure', v):
       my_fields.append(my_ticket[k - 1]) #started previous index at 1 for some reason
   
-  print(my_fields)
+  #print(my_fields)
   print(math.prod(my_fields))
 
 
@@ -135,6 +114,5 @@ def get_my_departure_product(my_ticket):
 
 t = time.time()
 find_categories_part_2(find_invalid(open_file()))
-get_my_departure_product([73,101,67,97,149,53,89,113,79,131,71,127,137,61,139,103,83,107,109,59])
-#print(find_invalid(open_file()))
+get_my_departure_product()
 print(f'took {time.time()-t} seconds')
